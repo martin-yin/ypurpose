aa8285d429ec24a0e721e2c5d504c4c6e15522e6
尹县伟 <1605074969@qq.com>
2023-10-29 22:06:57 +0800

    Delete ypurpose.config.ts

--- ypurpose.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from './src/config/index';
-import { gitFetchPull } from './src/plugins';
-
-export default defineConfig({
-  command: 'build',
-  options: {},
-  plugins: [gitFetchPull()]
-});
